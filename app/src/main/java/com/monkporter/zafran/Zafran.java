4aba0d15a7fcd2c7324ad539ce6cc1ec3fd6fd06
Sajal <sajaljain98@gmail.com>
2016-03-25 13:17:56 +0530

    Project Structure

---
+++ app/src/main/java/com/monkporter/zafran/Zafran.java
@@ -0,0 +1,10 @@
+package com.monkporter.zafran;
+
+import android.app.Application;
+
+/**
+ * Created by Sajal on 25-Mar-16.
+ */
+public class Zafran extends Application {
+
+}
---
+++ app/src/main/java/com/monkporter/zafran/ZafranMain/Splash.java
@@ -0,0 +1,15 @@
+package com.monkporter.zafran.ZafranMain;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.monkporter.zafran.R;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
