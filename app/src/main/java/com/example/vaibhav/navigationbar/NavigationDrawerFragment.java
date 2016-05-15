ebbdc39fe24780667e9601cf0abf1c323f9d96e0
Vabs123 <vbs2732gmail.com>
2016-05-15 11:36:02 +0530

    make basic Navigation Bar

---
+++ app/src/main/java/com/example/vaibhav/navigationbar/NavigationDrawerFragment.java
@@ -0,0 +1,68 @@
+package com.example.vaibhav.navigationbar;
+
+
+import android.app.ActionBar;
+import android.app.backup.SharedPreferencesBackupHelper;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.support.v7.widget.Toolbar;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class NavigationDrawerFragment extends Fragment {
+
+    public static final String PREF_FILE_NAME ="testpref";
+    private ActionBarDrawerToggle mDrawerToggle;
+    private DrawerLayout mDrawerLayout;
+
+    private boolean mUserLearnedDrawer;
+    private boolean mFromSavedInstanceState;
+
+    public NavigationDrawerFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_navigation_drawer, container, false);
+    }
+
+
+    public void setUp(DrawerLayout drawerLayout,Toolbar toolbar) {
+        mDrawerLayout = drawerLayout;
+        mDrawerToggle = new ActionBarDrawerToggle(getActivity(),drawerLayout,toolbar,R.string.drawer_open,R.string.drawer_close){
+
+            public void onDrawerOpened(View drawerView){
+                super.onDrawerOpened(drawerView);
+            }
+
+            public void onDrawerClosed(View drawerView){
+                super.onDrawerClosed(drawerView);
+            }
+        };
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+    }
+    public static void saveToPreferences(Context context,String preferenceName,String preferenceValue){
+        SharedPreferences sharedPreferences = context.getSharedPreferences(PREF_FILE_NAME,Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(preferenceName,preferenceValue);
+        editor.apply();
+    }
+
+    public static String readFromPreferences(Context context,String preferenceName,String defaultValue){
+        SharedPreferences sharedPreferences = context.getSharedPreferences(PREF_FILE_NAME,Context.MODE_PRIVATE);
+        return sharedPreferences.getString(preferenceName,defaultValue);
+    }
+}
