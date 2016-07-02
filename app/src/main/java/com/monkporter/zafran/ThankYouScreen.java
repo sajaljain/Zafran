33afd8609cee7331a561a32e3a8ead8be6065031
Vabs123 <vbs2732gmail.com>
2016-07-02 22:40:57 +0530

    Add Thank You Screen

---
+++ app/src/main/java/com/monkporter/zafran/ThankYouScreen.java
@@ -0,0 +1,43 @@
+package com.monkporter.zafran;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.monkporter.zafran.model.AddressDetailAttributes;
+
+public class ThankYouScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_thank_you_screen);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.tool_bar_id);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        menu.findItem(R.id.action_notification).setVisible(false);
+        menu.findItem(R.id.add_button_id).setVisible(false);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id == android.R.id.home){
+            this.finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
